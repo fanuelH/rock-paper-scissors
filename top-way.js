205cb6d6f75db578163f2191229becf7c396b897
Fanuel Hindeya <154921331+fanuelH@users.noreply.github.com>
2025-07-08 18:00:05 +0300

    feat: add scripting using TOP guideline

---
+++ top-way.js
@@ -0,0 +1,50 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  let option = ["Rock", "Paper", "Scissor"];
+  let randomChoice = Math.floor(Math.random() * 3);
+  console.log(`Computer's choice is ${option[randomChoice]}`);
+  return option[randomChoice];
+}
+
+function getHumanChoice() {
+  let option = { R: "Rock", P: "Paper", S: "Scissor" };
+  let humanChoice = prompt(
+    `Please Enter "R" for "Rock" or "P" for "Paper" or "S": "Scissor" `
+  );
+
+  if (humanChoice === null) return null;
+
+  humanChoice = humanChoice.trim().toUpperCase();
+
+  if (["R", "P", "S"].includes(humanChoice)) {
+    console.log(`Your choice is ${option[humanChoice]}`);
+    return option[humanChoice];
+  } else {
+    console.log("invalid Input. Please try Again!");
+    return getHumanChoice();
+  }
+}
+
+const HUMAN_CHOICE = getHumanChoice();
+const COMPUTER_CHOICE = getComputerChoice();
+
+function playRound() {
+  if (HUMAN_CHOICE === COMPUTER_CHOICE) {
+    console.log("It's a tie!");
+  } else if (
+    (HUMAN_CHOICE === "Rock" && COMPUTER_CHOICE === "Scissor") ||
+    (HUMAN_CHOICE === "Paper" && COMPUTER_CHOICE === "Rock") ||
+    (HUMAN_CHOICE === "Scissor" && COMPUTER_CHOICE === "Paper")
+  ) {
+    console.log("Congratulations. You won!");
+    humanScore++;
+  } else {
+    console.log(`Oops. you lost! ${COMPUTER_CHOICE} beats`);
+    computerScore++;
+  }
+  console.log(`Your Score: ${humanScore} and Computer ${computerScore}`);
+}
+
+playRound();
